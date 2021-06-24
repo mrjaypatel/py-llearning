047e66650d692314b1052778f58396042d25b33f
Jay Patel <p1998jay@gmail.com>
2021-06-24 23:02:50 +0530

    lesson-1/2

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+# name = "Jay Patel"
+
+# def main(_name):
+#     print("Hello ", _name)
+#     print("How Are you? is there anything I can do for you?")
+
+# #Check function exists and properly loaded or not!
+# if __name__ == "__main__":
+#     main(name)
+
+
+# def myFunc():
+#     print("Test me");
+    
+# myFunc()
+
+
+
